c88bca67fc4d1ae1877b095990b83d103e9d3f40
YenaLey <yena.e121@gmail.com>
2024-11-07 01:35:06 +0900

    Chore: 배경색 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import "@/App.css";
-import React, { createContext, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 
 export default function App() {
   return (
