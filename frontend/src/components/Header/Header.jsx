519263e3a96cffd7c04bce8fc233be128007e0da
ha11yeon <jaung7207@gmail.com>
2024-11-10 05:25:27 +0900

    feat: 로고 수정

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "./Header.css";
-import { Link } from "react-router-dom"; // 페이지 간 이동을 위한 Link 컴포넌트
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="list-logo">
-        <Link to="/main">
-          <img src={process.env.PUBLIC_URL + "/logo.png"} alt="logo" />
-        
+      <div className="header-container">
+        <Link to="/main" className="header-logo">
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="logo" />
         </Link>
+        
       </div>
-      <nav className="nav">
-        
-        
-      </nav>
     </header>
   );
 }
