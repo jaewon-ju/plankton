40ed57d985312931e304073566a99176ebf0bb89
Jaewon <20011211s@gmail.com>
2024-11-10 02:48:11 +0900

    Feat: info 수정

--- backend/src/main/java/plankton/backend/controller/InfoController.java
+++ backend/src/main/java/plankton/backend/controller/InfoController.java
@@ -12,13 +12,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import plankton.backend.dto.response.SuccessResponse;
 
-@RestController("/info")
+@RestController
+@RequestMapping("/info")
 public class InfoController {
 
     String temp = null;
