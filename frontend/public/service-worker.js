ce4d531024696cd29e9aca766b604b118dbeb05c
yezzero <yeyeonggim06@gmail.com>
2024-11-09 11:12:12 +0900

    Feat: 스플래쉬 화면 테스트

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,10 @@
+// main JavaScript file (e.g., index.js)
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, error => {
+        console.log('ServiceWorker registration failed: ', error);
+        });
+    });
+    }
