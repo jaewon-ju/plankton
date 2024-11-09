e09f4f9b50bb024067b2fdf4e2f6acaf30cf09d3
yezzero <yeyeonggim06@gmail.com>
2024-11-10 02:16:55 +0900

    Feat: 와글와글 페이지 추가

---
+++ frontend/src/components/WaggleChat/WaggleChat.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./WaggleChat.css";
+
+export default function WaggleChat() {
+    return (
+        <div className="wchat-container">
+            
+        </div>
+    )
+}
---
+++ frontend/src/components/WaggleReport/WaggleReport.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./WaggleReport.css";
+
+export default function WaggleReport() {
+
+    <div className="wreport-container">
+        <div className="wreport-category">
+            <p>사건 유형</p>
+            <div className="wreport-category-list">
+                <p>인원혼잡</p>
+                <p>통행불편</p>
+                <p>시설문제</p>
+                <p>안전사고</p>
+                <p>기타</p>
+            </div>
+        </div>
+
+        <div className="wreport-content">
+            <h1>신고내용</h1>
+            <input type="text" />
+        </div>
+
+        <div className="wreport-img">
+            <h1>사진 첨부하기</h1>
+            <input type="file" />
+        </div>
+
+        <button>접수하기</button>
+    </div>
+}
